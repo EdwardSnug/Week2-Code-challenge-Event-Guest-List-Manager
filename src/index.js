14ab6cdbf1cdf4791ecc2c6e44b4c8da56297258
Edward Karogo <prezokams@gmail.com>
2025-06-16 07:47:13 +0300

    SA WEEK 2 CHALLENGE EDIT

--- src/index.js
+++ src/index.js
@@ -3,10 +3,59 @@ let form = document.getElementById("myform")
 let listy = document.getElementById("guestnames")
 
 form.addEventListener("submit", (e) => {
+    //This prevents the browser from reloading
     e.preventDefault()
     let inputValue = document.getElementById("myinput").value.trim()
-    names.push(inputValue)
-    let list = document.createElement('li')
-    listy.appendChild(list)
-    list.textContent = inputValue
+    //Lets check if the guests are more than ten
+    if(names.length >= 10){
+        alert("Guest list reached, Kindly remove a guest(s) to be able to add more");
+        return;//Exit if the guest list exceeds 10
+    }
+    //Ensure there is valid input in our inputValue
+    if (inputValue === ""){
+        alert("Please Enter a valid guest name");
+    }
+    names.push(inputValue);
+    //create an li element to display our inputValues as a list
+    let listItem = document.createElement('li');
+    //Update our parent element and our new created child element 'li'
+    listy.appendChild(listItem);
+    listItem.textContent = inputValue;
+    //Lets create a button element for deleting any of our input guest:
+    let deletebtn = document.createElement("button");
+    //Adding an event listener to our button
+    deletebtn.addEventListener("click", () =>{
+        //Removing the clicked guest from our list
+        listy.removeChild(listItem);
+        //Updating our array using a filter method
+        //our filter method receives a callback of each name and keeps only the elements that satisfy the condition
+        //Every name is kept except the one we are deleting
+        //names = names.filter(function(name){
+        // return name !== inputValue
+        //});Lets write its arrow function:
+        names = names.filter(name => name !== inputValue);
+    })
+    deletebtn.textContent = "Remove Guest";
+    //Lets append button to the listItem parent
+    listItem.appendChild(deletebtn);
+    //Lets create a RSVP status(Default as Not Attending)
+    //Lets create a span element
+    let guestStatus = document.createElement('span');
+    guestStatus.textContent = "Not Attending";
+    //Update our parent listItem with its new child element guestStatus
+    listItem.appendChild(guestStatus);
+    //Now lets add a Toggle RSVP button
+    let rsvpBtn = document.createElement("button");
+    //Display in button Toogle RSVP
+    rsvpBtn.textContent = "Toggle RSVP";
+    //Finally lets add an eventlistener
+    rsvpBtn.addEventListener("click", () => {
+        if (guestStatus.textContent === "Not Attending") {
+            guestStatus.textContent = "I\'ll be Attending";
+        } else {
+            guestStatus.textContent = "Not Attending";
+        }
+    });
+    //Lets add our new btn element to our parent element
+    listItem.appendChild(rsvpBtn);
 })
